3f6da4f7438e2577db4bf06b325f6b4afc068f50
The_Sushi <64918853+Dev-Atsushi@users.noreply.github.com>
2021-02-18 10:50:49 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -4,5 +4,5 @@ import random
 
 while True:
     numero = random.randint(1,6)
-    print("Rolando dado...E seu número é ", numero)
+    print("Rolando dado...E seu número é", numero)
     input("Clique qualquer tecla para rolar novamente.")
